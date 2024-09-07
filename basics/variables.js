338d162f3f1e6d7c8c07c3cafe42876a86e6ad24
AshokMaghani <ashokmaghani1909@gmail.com>
2024-09-07 07:49:46 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 112233
+let accountName = "Ashok Kumar"
+var accountEmail = "ashok@gmail.com"
+
+accountCity = "Jodhpur"
+
+console.table([accountId, accountName, accountEmail, accountCity]);
+
+// accountId = 2233 
+
+accountName= "Ashok Maghani"
+accountEmail= "akd@gmail.com"
+accountCity = "Bengaluru" 
+
+console.table([accountId, accountName, accountEmail, accountCity]);
