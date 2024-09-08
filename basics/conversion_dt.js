eaa5585934fc51c903021e0b1b9c232ccc436c6a
AshokMaghani <ashokmaghani1909@gmail.com>
2024-09-08 06:56:56 +0000

    Conversion in Data Types

---
+++ basics/conversion_dt.js
@@ -0,0 +1,18 @@
+let score = "33abc";
+
+console.log(typeof score);
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+
+
+// *******  Operations **********
+
+console.log(1+2);
+console.log(1+2);
+console.log(1+2);
+console.log(1+2);
